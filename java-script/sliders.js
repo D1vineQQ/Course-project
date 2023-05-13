b70f82ce35e7e87c0acd59c7b31c792dd9816e3f
N1ce <4layse@mail.ru>
2023-05-13 07:35:13 +0300

    Сайт готов
    а я спать

--- java-script/sliders.js
+++ java-script/sliders.js
@@ -1,3 +1,33 @@
+$(function () {
+    let table = $('.footer'), tbody = "", fileAdress = "https://raw.githubusercontent.com/D1vineQQ/Course-project/Experimental/footer.xml";
+
+    const RequestObject = {
+
+        url: fileAdress,
+        method: 'GET',
+        dataType: 'xml',
+        async: true,
+        success: (resp) => {
+            $(resp).find("infoAbout").each((index, item) => {
+                tbody += `<tr>`;
+                tbody += `<td> ${item.children[0].textContent}`;
+                tbody += `${item.children[1].textContent}`;
+                tbody += `${item.children[2].textContent} </td>`;
+                tbody += `</tr>`;
+            })
+            table.html(tbody);
+        },
+        error: (error) => console.log(error)
+    }
+    $.ajax(RequestObject);
+});
+$(document).ready(function(){
+    $("#burger").click(function() { 
+        $(".nav-list").slideToggle();
+    });
+    
+});
+
 document.addEventListener("DOMContentLoaded", () => {
     const slider = new ItcSimpleSlider(".itcss", {
         autoplay: true,
@@ -9,28 +39,4 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector(".btn-next").onclick = () => {
         slider.next();
     };
-});
-
-$(function () {
-    let table = $('.footer'), tbody = "", fileAdress = "https://raw.githubusercontent.com/Danturador/testDar.io/main/GoW_III/DevelopersTable.xml";
-    
-    const RequestObject = {
-    
-        url: fileAdress,
-        method: 'GET',
-        dataType: 'xml',
-        async: true,
-        success: (resp) => {
-            $(resp).find("infoAbout").each((index, item) => {
-                tbody += `<tr>`;
-                    tbody += `<td> ${item.children[0].textContent} </td>`;
-                    tbody += `<td> ${item.children[1].textContent} </td>`;
-                    tbody += `<td> ${item.children[2].textContent} </td>`;
-                tbody += `</tr>`;
-            })
-            table.html(tbody);
-        },
-        error: (error) => console.log(error)
-    }
-    $.ajax(RequestObject);
 });
